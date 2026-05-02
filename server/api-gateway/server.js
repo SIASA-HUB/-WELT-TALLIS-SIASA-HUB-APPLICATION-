a2a44bb2e80de51c5390c30c9d24a453d9e49d5f
welt717 <mumohpeter2030@gmail.com>
2026-05-02 18:06:34 +0300

    ngnnix  configurations

--- server/api-gateway/server.js
+++ server/api-gateway/server.js
@@ -155,6 +155,9 @@ const createProxy = (targetUrl) => {
     selfHandleResponse: false,
     proxyTimeout: 900000,
     timeout: 900000,
+    pathRewrite: {
+      '^/api/v1': '/v1'
+    },
     on: {
       proxyReq: (proxyReq, req) => {
 
